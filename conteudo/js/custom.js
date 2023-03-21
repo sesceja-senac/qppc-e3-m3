efc3d2bb2d7f4881ec353a719a535f2d7a61c29a
brunaflores-gete <80692164+brunaflores-gete@users.noreply.github.com>
2023-03-21 10:55:34 -0300

    página 3 finalizada

--- conteudo/js/custom.js
+++ conteudo/js/custom.js
@@ -1,5 +1,20 @@
 $(function () {
 
   $('footer li.active').prevAll("li").addClass("visitado");
+
+  $('.painel-plano button').click(function(){
+    $(this).addClass('visitado');
+  });
+
+  $('.collapse-dado button').click(function(){
+    $('.collapse-dado button').siblings().removeClass('show');
+    $(this).siblings().toggleClass('show');
+
+    if($('.collapse-dado button').siblings().hasClass('show')){
+      $(this).siblings().removeClass('show');
+    }
+  })
+
+  $('.painel-estrategias button').click(function(){$(this).addClass('visitado')})
   
 });
