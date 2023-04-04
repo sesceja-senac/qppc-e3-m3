c000bd6ac8aa1c95032b4ac74525d46ac10faa5c
RenataLimaTED <relferreira@senacrs.com.br>
2023-04-04 17:20:12 -0300

    modificacao do objeto whatsapp, inclusao css, js e text html

---
+++ conteudo/obj-whats/js/script-obj-whats.js
@@ -0,0 +1,185 @@
+function carregaModalTranscricao() {
+    const descricaoAudio = {
+        transcricao1: {
+            nomeAudio: "Áudio Apresentação Newton:",
+            texto: "Oi, meu nome é Nilton Silva, eu sou jornalista e trabalho nessa conexão entre comunicação, arte, cultura desde meados dos anos 1980. Na época, eu integrava um grupo de teatro e também trabalhava como técnico de reportagens de uma televisão. No meu grupo de teatro, a gente precisava de uma pessoa para fazer produção, também para fazer a divulgação dos espetáculos e por conta de eu estar atuando nessas duas áreas, eu comecei a pegar para mim essa função. Então, foi a partir daí que eu passei a fazer divulgação das peças do meu grupo de teatro e, aos poucos, outros grupos de teatro e outros artistas me convidaram também para fazer a divulgação dos seus espetáculos, exposições, shows musicais. Eu ainda não estava estudando jornalismo e naquela época a gente escrevia o release, numa máquina de escrever, depois, alguns erros a gente colava correções em cima do release e tirava xerox e batia de porta em porta, de jornal em jornal, de televisão em televisão, de rádio em rádio, presencialmente, para divulgar os espetáculos. Depois disso, eu fui estudar jornalismo, abandonei as artes cênicas e comecei a atuar como jornalista dentro dos veículos, dentro de um jornal, dentro de uma rádio ou dentro de uma televisão, quer dizer, passei para o outro lado do balcão.",
+        },
+        transcricao2: {
+            nomeAudio: "Áudio Apresentação Giulia:",
+            texto: "Meu nome é Giulia, eu tenho 26 anos e danço desde os 3 anos de idade. Eu sou artista, bailarina, professora, produtora. Opero luz e som, também. Trabalho num grupo que se chama Macarenando Dance Concept, além disso, dou aula numa escola de inclusão, que se chama CID, e também dou aula numa escola de dança que se chama Balancê. Trabalho também com as mídias sociais, as redes sociais da Macarenando Dance Concept.",
+        },
+        transcricao3: {
+            nomeAudio: "Áudio Apresentação Diovana:",
+            texto: "Meu nome é Diovana Gheller, tenho 47 anos. Sou produtora de eventos independentes há 8 anos, produzia eventos no meu próprio bar, o Von Teese Bar, que fechou durante a pandemia; eventos de rua, como a Festa de Rua Bento Figueiredo; festivais, como o POA Burlesque, que teve quatro edições, e produzo eventos independentes de dança, enfim, de teatro. É isso.",
+        },
+        transcricao4: {
+            nomeAudio: "Áudio Newton:",
+            texto: "Houve também uma mudança na relação entre divulgadores e as emissoras de TV, os jornais e rádios, os veículos de comunicação. Essa mudança se deu a partir da Internet, então a divulgação começou a ser feita por e-mail, não precisava mais visitar as redações. Houve uma grande transformação na forma de produção e de divulgar espetáculos e outros eventos culturais. Dentro disso também entrou uma outra revolução, que foram as redes sociais, e, a partir disso, tanto jornalista que está na emissora acaba tendo que seguir os artistas e grupos de teatro e coletivos nas redes sociais, para saber o que está acontecendo, e, da mesma maneira, as redes sociais só funcionam como uma porta de divulgação. Uma outra revolução também aconteceu nesse meio tempo que foi a entrada da Internet e redes sociais. Então, nesse sentido, o jornalista que trabalhava nos veículos teve que procurar seguir artistas coletivos teatrais e grupos para também ter consciência do que está acontecendo, ficar sabendo do que está acontecendo e os grupos e os artistas encontraram nas redes sociais um outro mecanismo de divulgação dos seus trabalhos. Apesar dessa revolução, as mídias tradicionais, como rádio, TV, jornal, revistas, continuaram tendo uma grande importância, na medida que são espaços, além de contato direto com os seus leitores, ouvintes e telespectadores, também são espaços de validação do trabalho desse artista e acredito que não perderam a sua importância, continuam sendo muito importantes para a construção desse campo da cultura. Por outro lado, as redes sociais necessitam de um trabalho muito efetivo e de captar novos seguidores e de tornar e colocar conteúdos abrangentes e muito específicos também dentro dos seus canais de divulgação, para que esse público possa, além de saber que um espetáculo está acontecendo, conhecer bastidores, conhecer o perfil de cada um dos seus artistas. Ela é um grande portal de marketing que eu acredito que, para a cultura, é essencial. Há 4 anos eu também atuei como assessor de comunicação da Secretaria de Estado da Cultura, e, nesse lugar, eu tive a oportunidade de trabalhar diretamente com a confecção de releases desse órgão público e também de vídeos para as redes sociais. Nesse contexto, eu pude perceber o quanto é importante primeiro uma tentativa de crescimento orgânico das redes, em que você vai buscar novos seguidores a partir dos seguidores que você já tem, e, no caso do impulsionamento de postagens, a gente acabava não fazendo, porque dependia de licitação junto a agentes de publicidade, como é um órgão do governo... Então nosso crescimento sempre foi bastante orgânico e tentando trabalhar com algum tipo de conteúdo que chamasse ou que chame a atenção para o perfil. Hoje em dia, uma das grandes entregas que acontecem pelas redes sociais são os vídeos, no caso do Instagram, por exemplo, os Reels. O Twitter também é um local bastante importante no sentido de que ele tem essa especificidade de tratar o conteúdo de uma forma bastante resumida e você pode então entregar ao teu público uma informação bastante direta. Por fim, pensando nesses dois lados de atuação que eu tive a oportunidade e estou tendo ainda de trabalhar, tanto neste lado como assessor de comunicação de instituições ou de grupos e coletivos culturais, e também como jornalista que está dentro dos veículos, na TV, rádio e jornal, nessa trajetória, eu pude perceber o quanto são importantes as estratégias de comunicação e o uso e a atenção que cada vez que esse coletivo tenha um novo produto a divulgar, quais são hoje as ferramentas contemporâneas que você pode se utilizar para fazer com que esse trabalho chegue na ponta, no maior número de pessoas possíveis. Então, isso envolve o surgimento de novos canais e novas formas de divulgação, novas redes, então é um processo sempre em constante transformação. Acho que é isso. Obrigado. ",
+        },
+        transcricao5: {
+            nomeAudio: "Áudio Diovana:",
+            texto: "As plataformas digitais são basicamente o meio de comunicação para divulgar qualquer evento ou qualquer produto cultural, que a gente queira. Primeiro era o Facebook, depois se transformou no Instagram e depois veio o TikTok e assim vai mudando. Primeiro se divulgava muito com fotos, depois foi pedindo mais vídeos, então a gente vai se adequando ao que as mídias digitais vão pedindo para ter mais entrega. Tanto é que é o Instagram, por exemplo, já se deu conta de que às vezes a gente quer vender espetáculo, quer vender alguma coisa, e ele entrega menos, organicamente. Então precisa daquele patrocínio e ele deixa patrocinar certas coisas ou não quando ele entende que é uma venda, deixando também agora patrocinar vídeos, porque entende que a gente está fazendo aquilo para vender algum produto, porque basicamente é como está se vendendo ultimamente qualquer coisa no mercado, é através de mídias digitais e vídeos e influencers, que recomendam ou não recomendam, e pessoas que recomendam produtos, lugares e eventos e tudo mais. Então, é além das mídias originais e oficiais de jornais, telejornais, enfim, hoje em dia o que funciona são os vídeos das plataformas digitais das mais comuns, e é assim que a gente vem se adequando na produção independente para divulgação dos eventos culturais. ",
+        },
+        transcricao6: {
+            nomeAudio: "Áudio Giulia:",
+            texto: "Sobre as mídias digitais, eu sempre tento fazer uma análise antes do projeto, da peça, do espetáculo, para entender quais são as demandas que pedem para serem feitas nas mídias digitais também. Eu já entrei em contato com influencers daqui de Porto Alegre principalmente, para que se divulgue; já entrei também, no nosso caso da Macarenando, como a gente tem um espetáculo que ele tem sempre uma temática muito forte, no caso do Caetano, seja o Caetano Veloso, seja Roberto Carlos, a gente já entrou em contato com fã-clubes também, então, a partir disso, a gente fecha algum tipo de parceria para a divulgação, seja com influencers ou seja com fã-clube para divulgarem e compartilharem esse material, eu acho sempre muito importante. Acho muito importante ter um site também onde se concentre as informações gerais do grupo, como uma basezinha mesmo para que qualquer momento que a pessoa procure sobre isso, ela tenha um fácil acesso. A gente geralmente usa muito o Instagram. Também a gente já usou a ferramenta de patrocinar, de divulgar por isso, e sempre funcionou muito bem aqui em Porto Alegre, sabendo qual é o teu público. No caso do nosso público, por exemplo, do espetáculo do Caetano, é uma galera mais jovem que curte teatro, que curte cinema. Acho que isso é principalmente uma análise muito importante para se utilizar nesse tipo de divulgação, das mídias digitais e de saber qual é o teu público e conseguir fazer essa persona para conseguir atingir ele de uma forma mais fácil.",
+        },
+        transcricao7: {
+            nomeAudio: "Áudio Giulia:",
+            texto: "Sobre as medidas exteriores, para a gente que trabalha com peça de teatro, é muito difícil conseguir ter grana para imprimir material e para botar outdoor, o que eu acho que seria incrível um outdoor, mas é inviável pela situação que a gente vive e pelos custos. Então, a gente sempre procura não utilizar isso e focar energia em outras mídias, focar bastante na digital, principalmente, e nas tradicionais também. Então, a gente não utiliza muito essas mídias exteriores, mas eu imagino que dê certo, que seja uma estratégia, se tu tem equipe de trabalho para isso e se tu tem o financeiro para isso, eu acredito que dê certo. ",
+        },
+        transcricao8: {
+            nomeAudio: "Áudio Giulia:",
+            texto: "Para essa mídia orgânica, eu sempre ligo ela muito com as mídias digitais, porque eu acredito que hoje em dia estejam um pouco atreladas também, acho que elas podem se complementar, que é utilizar então essa ferramenta das mídias digitais como uma proposta para a gente conseguir fazer circular essa informação orgânica. Então, no momento em que tu faz uma postagem no Instagram, tu estimula o teu público a comentar, compartilhar, a chamar um amigo nas próprias postagens, tu acaba atingindo, por via de uma mídia orgânica também mais público. E é sempre muito importante a finalização de cada peça, ter sempre uma conversa muito estabelecida com o teu público, seja em postagem, seja numa recepção, para que esse público sinta vontade e necessidade de “espalhar a palavra da peça”. Porque eu acho que faz toda a diferença esse encerramento, seja uma publicação de “obrigado”, seja tu estar lá para recepcionar o teu público, seja manter então essa rede, é sempre muito importante. Porque essa basezinha, esse chão que vem antes, que é essa fidelização de público, vai servir muito para a mídia orgânica, porque é daí que vem o boca a boca, no momento em que tu já fidelizou também o teu público e tu pode instigar, de uma forma não orgânica, a acontecer a mídia orgânica. ",
+        },
+        transcricao9: {
+            nomeAudio: "Áudio Diovana:",
+            texto: "O impulsionamento, no primeiro caso, serve mais para a gente expandir e explodir essa bolha e sair um pouco mais dessa mídia orgânica que atinge mais um pequeno público que é o do círculo daquele meio, que é muito importante, porque através do boca a boca e do compartilhamento orgânico de pessoas que realmente se interessam por aqueles eventos e realmente recomendam aqueles eventos, ele quase que é mais eficiente e mais eficaz do que qualquer impulsionamento. Porque, na verdade, o boca a boca valida mais qualquer evento do que uma propaganda possa validar, uma propaganda fake ou influencer que foi contratado para aquilo, ou impulsionamento: “paguei para chegar aquilo naquela pessoa”. Então a mídia orgânica e o boca a boca é, na minha opinião, o mais eficiente em captação de clientes por ser o mais verdadeiro, por ser o mais engajado. Então, o engajamento orgânico geralmente atrai mais clientes do que o pago, justamente pela verdade que ele traz e pelo afinco com que o engajamento se dá, é quase que comparado com a qualidade do produto cultural que está se colocando à venda. Então são pessoas reais que consomem aquele produto realmente e recomendam aquele produto, então, na minha opinião, ele é até mais eficaz.",
+        },
+    };
+
+    const whatsapp = (params) => {
+        return descricaoAudio[params];
+    };
+
+    const modalTranscricao = (params) => {
+        $("#exampleModal").remove();
+
+        var modal = `<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel"> ${params.nomeAudio}</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+          ${params.texto}
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+          </div>
+        </div>
+      </div>
+    </div>`;
+
+        $("body").append(modal);
+        $("#exampleModal").modal("show");
+    };
+
+    $(document).on("click", ".txtAudio", function (e) {
+        modalTranscricao(whatsapp($(this).attr("id")));
+    });
+}
+
+function mostraTodas() {
+    var index = 0;
+    var divs = $('.sequencial');
+
+    $('#mostraTudo').click(function () {
+        if (index < divs.length) {
+            divs.eq(index).fadeIn(1000);
+            index++;
+        }
+        if (index == divs.length) {
+            $('#mostraTudo').hide();
+        }
+    });
+}
+
+function mostraMensagemIndividual() {
+    $('#audioNewton1').on('ended', function () {
+        $('#carregando').show().delay(1000).fadeOut(1000);
+        setTimeout(function () {
+            $('#giulia1').show();
+        }, 3000);
+    });
+
+    $('#audioGiulia1').on('ended', function () {
+        $('#carregando').show().delay(1000).fadeOut(1000);
+        setTimeout(function () {
+            $('#diovana1').show();
+        }, 3000);
+    });
+
+    $('#audioDiovana1').on('ended', function () {
+        $('#digitando').show().delay(1000).fadeOut(1000);
+        setTimeout(function () {
+            $('#prod1').show();
+        }, 3000);
+        setTimeout(function () {
+            $('#carregando').show().delay(1000).fadeOut(1000);
+        }, 4000);
+
+        setTimeout(function () {
+            $('#newton2').show();
+        }, 6000);
+    });
+
+    $('#audioNewton2').on('ended', function () {
+        $('#digitando').show().delay(1000).fadeOut(1000);
+        setTimeout(function () {
+            $('#prod2').show();
+        }, 3000);
+        setTimeout(function () {
+            $('#carregando').show().delay(1000).fadeOut(1000);
+        }, 4000);
+
+        setTimeout(function () {
+            $('#diovana2').show();
+        }, 6000);
+    });
+
+    $('#audioDiovana2').on('ended', function () {
+        $('#carregando').show().delay(1000).fadeOut(1000);
+        setTimeout(function () {
+            $('#giulia2').show();
+        }, 3000);
+    });
+
+    $('#audioGiulia2').on('ended', function () {
+        $('#digitando').show().delay(1000).fadeOut(1000);
+        setTimeout(function () {
+            $('#prod3').show();
+        }, 3000);
+        setTimeout(function () {
+            $('#carregando').show().delay(1000).fadeOut(1000);
+        }, 4000);
+
+        setTimeout(function () {
+            $('#giulia3').show();
+        }, 6000);
+    });
+
+    $('#audioGiulia3').on('ended', function () {
+        $('#digitando').show().delay(1000).fadeOut(1000);
+        setTimeout(function () {
+            $('#prod4').show();
+        }, 3000);
+        setTimeout(function () {
+            $('#carregando').show().delay(1000).fadeOut(1000);
+        }, 4000);
+
+        setTimeout(function () {
+            $('#giulia4').show();
+        }, 6000);
+    });
+
+    $('#audioGiulia4').on('ended', function () {
+        $('#carregando').show().delay(1000).fadeOut(1000);
+        setTimeout(function () {
+            $('#diovana4').show();
+        }, 3000);
+    });
+
+    $('#audioDiovana4').on('ended', function () {
+        $('#digitando').show().delay(1000).fadeOut(1000);
+        setTimeout(function () {
+            $('#prod5').show();
+        }, 3000);
+        $('#mostraTudo').hide();
+    });
+}
+
+mostraTodas();
+mostraMensagemIndividual();
+carregaModalTranscricao();
